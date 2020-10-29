bcb423560d078fd1983f40a2f6ae1a6987df1ed2
Z-Flint <68733689+Z-Flint@users.noreply.github.com>
2020-10-29 09:01:09 +0100

    Add files via upload

---
+++ account.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style4.css"/>
+    <title>Account page</title>
+</head>
+<body>
+<header>
+      <div id="menu-bar">
+			<div id="menu" onclick="onClickMenu()">
+				<div id="bar1" class="bar"></div>
+				<div id="bar2" class="bar"></div>
+				<div id="bar3" class="bar"></div>
+      </div>
+        <ul class="nav" id="nav">
+          <li><a href="./index.php">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="./narcos2.php">Nouveauté</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+        </div>
+        </div>
+        <div class="recherche">
+        <input type="search" name="Search" placeholder="Search">
+      </div>
+  </header>
+  <div class="menu-bg" id="menu-bg"></div>
+    <script src="./JS/script.js"></script>
+    
+</body>
+</html>
---
+++ narcos2.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="./css/style2.css" />
+      <title>Page de présentation</title>
+</head>
+<body>
+<header>
+<div id="menu-bar">
+			<div id="menu" onclick="onClickMenu()">
+				<div id="bar1" class="bar"></div>
+				<div id="bar2" class="bar"></div>
+				<div id="bar3" class="bar"></div>
+      </div>
+        <ul class="nav" id="nav">
+          <li><a href="./index.php">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="./narcos2.php">Nouveauté</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        </div>
+        <div class="container">
+        <div class="item">
+        <a href="./connexion.php">Connexion</a>
+      </div>
+        </div>
+        </div>
+        <div class="recherche">
+        <input type="search" name="Search" placeholder="Search">
+  </header>
+  <div class="menu-bg" id="menu-bg"></div>
+    <script src="./JS/script.js"></script>
+      <div class="pimg1">
+        <div class="ptext">
+          <span class="border">
+          European flavor
+          </span>
+        </div>
+      </div>    
+      <section class="section-black">
+      <div class="image">
+        <img class="image__img" src="./image/europe.png" alt="Bricks">
+        <div class="image__overlay image__overlay--primary">
+            <div class="image__title">Bricks</div>
+            <p class="image__description">
+            Lorem ipsum dolor sit amet consectetur</br> adipisicing elit. 
+            facilis doloremque atque eos et quos</br> debitis doloribus ipsa quam corporis? </br>Neque aspernatur, 
+            non veniam cupiditate</br> minus libero dolores earum nisi.</p>
+        </div>
+    </div>
+</section>
+    <div class="pimg2">
+      <div class="ptext">
+        <span class="border">
+        Americain Latine flavor
+        </span>
+      </div>
+    </div>
+    <section class="section-pink">
+      <div class="image">
+        <img class="image__img" src="./image/latine.png" alt="Bricks">
+        <div class="image__overlay image__overlay--primary">
+            <div class="image__title">Bricks</div>
+            <p class="image__description">
+            Lorem ipsum dolor sit amet consectetur</br> adipisicing elit. 
+            facilis doloremque atque eos et quos</br> debitis doloribus ipsa quam corporis? </br>Neque aspernatur, 
+            non veniam cupiditate</br> minus libero dolores earum nisi.</p>
+        </div>
+    </div>
+</section>
+    <div class="pimg3">
+      <div class="ptext">
+        <span class="border">
+        Oriental Flavor
+        </span>
+      </div>
+    </div>
+    <section class="section-yellow">
+      <div class="image">
+        <img class="image__img" src="./image/arabic.png" alt="Bricks">
+        <div class="image__overlay image__overlay--primary">
+            <div class="image__title">Bricks</div>
+            <p class="image__description">
+            Lorem ipsum dolor sit amet consectetur</br> adipisicing elit. 
+            facilis doloremque atque eos et quos</br> debitis doloribus ipsa quam corporis? </br>Neque aspernatur, 
+            non veniam cupiditate</br> minus libero dolores earum nisi.</p>
+        </div>
+    </div>
+</section>
+    <div class="pimg1">
+      <div class="ptext">
+        <span class="border">
+          Traversy Media
+        </span>
+      </div>
+    </div>
+    <section class="section-purple">
+      <div class="image">
+        <img class="image__img" src="./image/asie.png" alt="Bricks">
+        <div class="image__overlay image__overlay--primary">
+            <div class="image__title">Bricks</div>
+            <p class="image__description">
+            Lorem ipsum dolor sit amet consectetur</br> adipisicing elit. 
+            facilis doloremque atque eos et quos</br> debitis doloribus ipsa quam corporis? </br>Neque aspernatur, 
+            non veniam cupiditate</br> minus libero dolores earum nisi.</p>
+        </div>
+    </div>
+</section>
+    <div class="pimg1">
+      <div class="ptext">
+        <span class="border">
+          Traversy Media
+        </span>
+      </div>
+    </div>
+    <section class="section-white">
+      <div class="image">
+        <img class="image__img" src="./image/africa.png" alt="Bricks">
+        <div class="image__overlay image__overlay--primary">
+            <div class="image__title">Bricks</div>
+            <p class="image__description">
+            Lorem ipsum dolor sit amet consectetur</br> adipisicing elit. 
+            facilis doloremque atque eos et quos</br> debitis doloribus ipsa quam corporis? </br>Neque aspernatur, 
+            non veniam cupiditate</br> minus libero dolores earum nisi.</p>
+        </div>
+    </div>
+</section>  
+</body>
+</html>
